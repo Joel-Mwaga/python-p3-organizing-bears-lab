7214f235b0085eb76802c617fb78ad8cc9713b2b
Joel Mwaga <mwagahjoel14@gmail.com>
2025-05-21 10:37:01 +0300

    learnt organizing using sql

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES 
+("Mr. Chocolate", 20, "M", "White", "calm", 1),
+("Rowdy",10, "F", "Brown", "agitated", 1),
+("Tabitha", NULL, "M", "Black", NULL, NULL),
+("Sergeant", 5, "M", "Brown", "Jovial", 1),
+("Melissa", 14, "F", "Black", "Chill", 1) ,
+("Grinch", 25, "M", "White", "angry", 0),
+("Wendy", 13, "F", "Brown", "Melancholic", 1),
+(NULL, 5, "M", "Black", NULL, NULL)
